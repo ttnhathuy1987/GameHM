b56abc68d76b991476c14e8f7783b5e830931637
huy.truong <huy.truong@s3corp.com.vn>
2021-06-10 16:24:01 +0700

    First Commit

---
+++ GameHM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameHM
+//
+//  Created by s3 on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameHM/GameHMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameHMApp.swift
+//  GameHM
+//
+//  Created by s3 on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GameHMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
